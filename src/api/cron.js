0305998e7caf336b9bc29fbc5018bfeec39178c0
Ravi1470 <vk8580318@gmail.com>
2025-12-12 17:34:04 +0530

    cron job completed

--- src/api/cron.js
+++ src/api/cron.js
@@ -2,21 +2,27 @@ import { sendNotification } from "../lib/send-notifications.js";
 
 export default async function handler() {
   try {
-    const backendRes = await fetch(process.env.SPRING_API_URL, {
-      method: "GET",
-      headers: {
-        "x-api-key": process.env.CRON_API_KEY,
-        "Content-Type": "application/json",
-      },
-    });
+    const backendRes = await fetch(
+      `${process.env.SPRING_API_URL}/api/notification-data`,
+      {
+        method: "GET",
+        headers: {
+          "x-api-key": process.env.CRON_API_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
     const users = await backendRes.json();
     for (const user of users) {
       const { status, token } = user;
+      console.log(status);
+      console.log(token);
       const completed = status.COMPLETED || 0;
       const skipped = status.SKIPPED || 0;
+      const pending = status.PENDING || 0;
 
       const title = "Today's Habit Summary";
-      const body = `Completed: ${completed}, Skipped: ${skipped}`;
+      const body = `Completed: ${completed}, Skipped: ${skipped},pending:${pending}`;
       for (const t of token) {
         await sendNotification(t, title, body);
       }
--- src/server.js
+++ src/server.js
@@ -2,15 +2,31 @@ import express from "express";
 import dotenv from "dotenv";
 import handler from "./api/cron.js";
 import nodeCron from "node-cron";
+import { ping } from "./api/ping.js";
 
 dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`App running.. at port ${PORT}`);
-  nodeCron.schedule("*/1 * * * *", () => {
-    console.log("Running cron job...");
-    handler();
-  });
+// Helper function to register repeated cron schedules
+function registerCron(time, label, job) {
+  nodeCron.schedule(
+    time,
+    async () => {
+      console.log(`Running cron → ${label}`);
+      await job();
+    },
+    { timezone: process.env.TIMEZONE }
+  );
+}
+
+app.listen(PORT, () => {
+  console.log(`App running at port ${PORT}`);
+
+  registerCron("*/10 * * * *", "Ping", ping);
+
+  registerCron(process.env.CRON_TIME_1, "CRON_TIME_1", handler);
+  registerCron(process.env.CRON_TIME_2, "CRON_TIME_2", handler);
+  registerCron(process.env.CRON_TIME_3, "CRON_TIME_3", handler);
 });
